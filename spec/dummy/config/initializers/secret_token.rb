f983dd026926926a3fe522bd03e789be218ccbbc
Justin Coyne <justin@curationexperts.com>
2013-04-12 22:04:55 -0500

    Remove dummy app

--- spec/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '5b961940c05a37614119fb0cb2e0cb45331ca0a28cf9310b586243205939f7c6d97fc72a47419bc7bcdbfa6796a9409ad6078c496ffe9c75fc96ecdd0b2a7fee'
