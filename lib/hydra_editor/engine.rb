e00004bfe913e62ff8613bc4413c638b83e3f18d
Justin Coyne <justin@curationexperts.com>
2013-04-15 13:46:31 -0500

    Require the concerns directories

--- lib/hydra_editor/engine.rb
+++ lib/hydra_editor/engine.rb
@@ -1,6 +1,8 @@
 module HydraEditor
   class Engine < ::Rails::Engine
     engine_name 'hydra_editor'
-    config.paths.add "app/helpers/concerns", eager_load: true
+    config.paths.add "app/helpers/concerns",      eager_load: true
+    config.paths.add "app/controllers/concerns",  eager_load: true
+    config.paths.add "app/models/concerns",       eager_load: true
   end
 end
